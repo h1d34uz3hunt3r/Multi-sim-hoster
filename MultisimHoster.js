d207b67a216efe7f30de31dc5d312d1387be9238
h1d34uz3hunt3r <110579587+h1d34uz3hunt3r@users.noreply.github.com>
2025-11-13 21:32:25 -0600

    Update MultisimHoster.js

--- MultisimHoster.js
+++ MultisimHoster.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Multi-sim hoster
 // @namespace    https://github.com/h1d34uz3hunt3r
-// @downloadURL  https://raw.githubusercontent.com/h1d34uz3hunt3r/Multi-sim-hoster/main/MultisimHoster.js
-// @updateURL    https://raw.githubusercontent.com/h1d34uz3hunt3r/Multi-sim-hoster/main/MultisimHoster.js
+// @downloadURL  https://github.com/h1d34uz3hunt3r/Multi-sim-hoster/raw/main/MultisimHoster.js
+// @updateURL    https://github.com/h1d34uz3hunt3r/Multi-sim-hoster/raw/main/MultisimHoster.js
 // @version      2025-11-13
 // @description  Easier tribute loading for the old sim, the new sim, and murder games
 // @author       h1d34uz3hunt3r
@@ -815,4 +815,5 @@
     }
 
 })();
+
 
